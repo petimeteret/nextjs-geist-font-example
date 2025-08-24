d7b1e51b3a4f464d8ad81311a455804870595e42
petimeteret <66852979+petimeteret@users.noreply.github.com>
2025-08-24 23:08:36 +0200

    Add .next/static/chunks/src_app_articles_[id]_page_tsx_f27e8410._.js 1756069714797

---
+++ .next/static/chunks/src_app_articles_[id]_page_tsx_f27e8410._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/_1646614e._.js"
+],
+    source: "dynamic"
+});
